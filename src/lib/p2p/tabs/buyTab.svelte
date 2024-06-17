7f267448e5275002449d504dc869aad0197086f9
preciousken <pkehinde27@gmail.com>
2024-06-17 20:17:52 +0100

    added the p2p

--- src/lib/p2p/tabs/buyTab.svelte
+++ src/lib/p2p/tabs/buyTab.svelte
@@ -10,8 +10,8 @@
 
 <section class="_SideNav_1iwpl_9">
   <div class="_sideBox_1iwpl_28">
-    <span class="_active_1iwpl_71 _up_1iwpl_75" on:click={() => handleClick('buy')}>Buy</span>
-    <span class="_down_1iwpl_83" on:click={() => handleClick('sell')}>Sell</span>
+    <button class="_active_1iwpl_71 _up_1iwpl_75" on:click={() => handleClick('buy')}>Buy</button>
+    <button class="_down_1iwpl_83" on:click={() => handleClick('sell')}>Sell</button>
   </div>
   <em></em>
   <div class="_coinBox_1iwpl_91">
--- src/lib/p2p/tabs/sellTab.svelte
+++ src/lib/p2p/tabs/sellTab.svelte
@@ -10,10 +10,10 @@
 
 <section class="_SideNav_1iwpl_9">
   <div class="_sideBox_1iwpl_28">
-    <span class="_up_1iwpl_75" on:click={() => handleClick("buy")}>Buy</span
-    ><span
+    <button class="_up_1iwpl_75" on:click={() => handleClick("buy")}>Buy</button
+    ><button
       class="_active_1iwpl_71 _down_1iwpl_83"
-      on:click={() => handleClick("sell")}>Sell</span
+      on:click={() => handleClick("sell")}>Sell</button
     >
   </div>
   <em></em>
