fd0440b305a26df844f87bd818de90a3c6228b0b
highscoretech <highscoretechng@gmail.com>
2024-06-17 14:52:58 -0500

    okay

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,12 +1,11 @@
 import { browser } from '$app/environment'
-import { otp } from "../lib/store/otp"
+import { otp } from "$lib/store/otp"
 const id = browser && JSON.parse(localStorage.getItem('user'))
 
-const preloaed = browser && JSON.parse(localStorage.getItem('preload'))
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ route }) {
   let fach = browser && JSON.parse(sessionStorage.getItem('token'));
   otp.set(fach)
-  return {route : route.id, token: id , preloaed}
+  return {route : route.id, token: id }
 }
