81787af05da8c61aec027749366770dfaa626d94
wizjamin <jaminwizzy@gmail.com>
2024-07-26 23:16:51 +0100

    Updates

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -1,5 +1,12 @@
+import { browser } from '$app/environment'
 export const ServerURl = () => {
-  // return "http://localhost:8000";
+  if (browser) {
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+      return "http://localhost:8000";
+    } else {
+      return "https://ezcryptox.onrender.com";
+    }
+  }
   return "https://ezcryptox.onrender.com";
 };
 
