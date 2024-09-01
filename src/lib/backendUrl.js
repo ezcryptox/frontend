53410d7647b1499df1cf2e8da17e58404b2fd626
wizjamin <jaminwizzy@gmail.com>
2024-09-01 08:32:00 +0100

    Fixed kline update on chart

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -1,13 +1,13 @@
 import { browser } from '$app/environment'
 export const ServerURl = () => {
   let url = "https://backend-6diz.onrender.com";
-  if (browser) {
-    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-      url = "http://localhost:8000";
-    } else {
-      url = "https://backend-6diz.onrender.com";
-    }
-  }
+  // if (browser) {
+  //   if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  //     url = "http://localhost:8000";
+  //   } else {
+  //     url = "https://backend-6diz.onrender.com";
+  //   }
+  // }
   return url
 };
 
