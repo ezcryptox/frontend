558fc9786a0079b2f56b19dbb25ddbcd74135ea7
wizjamin <jaminwizzy@gmail.com>
2024-09-02 20:47:56 +0100

    Backend Url update

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -1,11 +1,11 @@
 import { browser } from '$app/environment'
 export const ServerURl = () => {
-  let url = "https://backend-6diz.onrender.com";
+  let url = "https://backend-goho.onrender.com";
   if (browser) {
     if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
       url = "http://localhost:8000";
     } else {
-      url = "https://backend-6diz.onrender.com";
+      url = "https://backend-goho.onrender.com";
     }
   }
   return url
